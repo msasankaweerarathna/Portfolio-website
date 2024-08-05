b66a69035a17d68cc6de97521a8a8b2f700b1dab
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-08-05 11:52:26 +0530

    Added typing animation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing Animation 
+var typed = new Typed(".typing", {
+    strings:[" ", "Web Designer", "UI/UX Designer", "Web Developer"],
+    typeSpeed:50,
+    backSpeed:60,
+    loop:true
+})
